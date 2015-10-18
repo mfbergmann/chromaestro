a40992e92e49e60037f31eb109ce32f7f0acbbce
Michael F Bergmann <michael.bergmann@yale.edu>
2015-10-18 16:36:48 -0700

    Initial Commit

---
+++ Chromaestro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chromaestro
+//
+//  Created by Michael F Bergmann on 10/18/15.
+//  Copyright © 2015 BergARTS. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
