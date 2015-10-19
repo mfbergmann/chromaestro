f70500807427732138e9807352ce9dce4a4ccdc6
Michael F Bergmann <michael.bergmann@yale.edu>
2015-10-18 18:07:13 -0700

    Playback of an hardcoded video

--- Chromaestro/ViewController.swift
+++ Chromaestro/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import Cocoa
+import AVFoundation
+import AVKit
+
+
+
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let url = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Placeholder", ofType:"m4v")!)
+        
+        let videoPlayer = AVPlayer(URL: url)
+        let playerView = AVPlayerView()
+        playerView.player = videoPlayer
+        self.view = playerView
+        
+       
+        
+        
         // Do any additional setup after loading the view.
     }
 
